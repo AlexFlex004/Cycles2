6374965204b48c760557cd4dc47ad0ddde610c95
Himawari Lad <Sacrificial.animal@gmail.com>
2025-06-22 03:08:40 +0300

    Code edited

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+
         //CASE1//
         System.out.println("CASE 1");
         int result = 2_459_000;
@@ -12,8 +13,9 @@ public class Main {
             month++;
             int total = month * savings;
             System.out.println("Месяц " + month + ", сумма накоплений равна " + total + " рублей");
-        System.out.println("     ");
+            System.out.println("     ");
         }
+
 
         //CASE2//
         System.out.println("CASE 2");
@@ -28,6 +30,7 @@ public class Main {
             System.out.print(startTwo + " ");
         }
         System.out.println("     ");
+
 
         //CASE3//
         System.out.println("     ");
@@ -45,6 +48,7 @@ public class Main {
             System.out.println("     ");
         }
 
+
         //CASE4//
         System.out.println("     ");
         System.out.println("CASE 4");
@@ -53,38 +57,83 @@ public class Main {
         float percent = 0.07F;
         int monthTwo = 0;
 
-        while (money < resultTwo) {
+        while (money < 12_000_000) {
             monthTwo++;
             money = (int) (money + money * percent);
             System.out.println("Месяц " + monthTwo + ", сумма накоплений равна " + money + " рублей");
-            System.out.println("     ");
         }
+        System.out.println("     ");
+
 
         //CASE5//
         System.out.println("CASE 5");
-        int resultThree = 12_000_000;
-        int moneyTwo = 15000;
-        float percentTwo = 0.07F;
-        int monthThree = 0;
-
-        while (moneyTwo < resultThree) {
+        money = 15000;
+        monthTwo = 0;
+        while (money < 12_000_000) {
+            money = (int) (money + money * percent);
             monthTwo++;
-            moneyTwo = (int) (moneyTwo + moneyTwo * percentTwo);
 
-            if (monthThree % 6 == 0) {
-                System.out.println("Месяц " + monthThree + ", сумма накоплений равна " + moneyTwo + " рублей");
-        System.out.println("     ");
+            if (monthTwo % 6 == 0) {
+                System.out.println("Месяц " + monthTwo + ", сумма накоплений равна " + money + " рублей");
+                System.out.println("     ");
             }
-
-        //CASE6//
-
-
-
         }
 
 
+        //CASE6//
+        System.out.println("CASE 6");
+        money = 15000;
+        monthTwo = 0;
+        int year = 0;
+        for (year = 0; year <= 9; year = year + 1) {
+            while (year < 9) {
+                money = (int) (money + money * percent);
+                monthTwo++;
 
+
+                if (monthTwo % 6 == 0) {
+                    System.out.println("Месяц " + monthTwo + ", сумма накоплений равна " + money + " рублей");
+                    System.out.println("     ");
+                    break;
+                }
+
+            }
+        }
+
+
+        //CASE7//
+        System.out.println("CASE 7");
+        int friday = 5;
+        int day = 0;
+        for (day = 0; day <= 31; day = day + 1) {
+            while (day < 31) {
+                day++;
+
+                if (day % friday == 0) {
+                    System.out.println("Сегодня пятница, " + day + "-е число. Необходимо подготовить отчет");
+                    System.out.println("     ");
+                    break;
+                }
+            }
+        }
+
+
+        //CASE8//
+        System.out.println("CASE 8");
+        int currentYear = 2025;
+        int pastYear = currentYear - 200;
+        int futureYear = currentYear + 100;
+
+        for (int cometYear = 0; cometYear <= currentYear; cometYear += 79) {
+            if (cometYear >= pastYear) {
+                System.out.println(cometYear);
+            }
+        }
+        for (int cometYear = currentYear + 1; cometYear <= futureYear; cometYear++) {
+            if (cometYear % 79 == 0) {
+                System.out.println(cometYear);
+                break;
+            }
+        }
     }
-
-
 }
